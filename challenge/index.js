719edf72789866f0ccb62b35553695d6bec021dd
Derek Houck <derek@derekhouck.com>
2018-12-06 12:08:59 -0800

    Add item functionality

--- challenge/index.js
+++ challenge/index.js
@@ -3,17 +3,38 @@
 
 
 function addItem(item) {
-    console.log(item);
+  const listElement = `
+    <li>
+      <span class="shopping-item">${item}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>
+    `;
+  return listElement;
+}
+
+function submitItem() {
+  event.preventDefault();
+  const item = $('.js-shopping-list-entry').val();
+  $('.shopping-list').prepend(addItem(item));
 }
 
 function main() {
+  //listen for add item button
+  $('.js-shopping-list-entry').keydown(function(e) {
+    if (e.which === 13) {
+      submitItem();
+      return false;
+    }
+  });
+  $('#js-shopping-list-form').submit(submitItem);
 
-//listen for add item button
-$('.js-shopping-list-form').on('submit', function(event) {
-    event.preventDefault();
-    console.log("hello, are we on?");
-    addItem('fruits');
-});
     
 }
 $(main());
