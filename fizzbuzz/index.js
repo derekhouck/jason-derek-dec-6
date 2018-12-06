3a9802cb62d41bdee640babc3b84452c00d5f1f6
Derek Houck <derek@derekhouck.com>
2018-12-06 13:07:55 -0800

    Furter FizzBuzz work

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,10 +1,39 @@
 'use strict';
 
-function fizzBuzz() {
+function fizzBuzz(num) {
+  if (num % 15 === 0) {
+    return 'fizzbuzz';
+  }
+  if (num % 5 === 0) {
+    return 'buzz';
+  }
+  if (num % 3 === 0) {
+    return 'fizz';
+  }
+  return num;
+}
+
+function makeArr(num) {
+  const arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+function fizzArr(arr) {
+  return arr.map(num => fizzBuzz(num));
+}
+
+function main() {
   $('#number-chooser').on('submit', function() {
     event.preventDefault();
     console.log('Form submitted');
   });
+
+  makeArr(15);
+
+  console.log(fizzArr([1, 2, 3, 4, 5]));
 }
 
-$(fizzBuzz());
+$(main());
